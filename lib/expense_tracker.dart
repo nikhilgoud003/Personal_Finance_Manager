89d44a27806134f6c83467370142b6c4d88c096d
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-03-29 11:53:23 -0400

    Implemented ExpenseTracker with transaction management and chart visualization

---
+++ lib/expense_tracker.dart
@@ -0,0 +1,219 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+import './widgets/new_transaction_form.dart';
+import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
+import './widgets/circular_chart.dart';
+import './models/transaction.dart';
+import './helpers/database_helper.dart';
+
+class ExpenseTracker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Transaction> _userTransactions = [];
+  bool _showChart = false;
+
+  List<Transaction> get _recentTransactions {
+    DateTime lastDayOfPrevWeek = DateTime.now().subtract(Duration(days: 6));
+    lastDayOfPrevWeek = DateTime(
+        lastDayOfPrevWeek.year, lastDayOfPrevWeek.month, lastDayOfPrevWeek.day);
+    return _userTransactions.where((element) {
+      return element.txnDateTime.isAfter(
+        lastDayOfPrevWeek,
+      );
+    }).toList();
+  }
+
+  _MyHomePageState() {
+    _updateUserTransactionsList();
+  }
+
+  void _updateUserTransactionsList() {
+    Future<List<Transaction>> res =
+        DatabaseHelper.instance.getAllTransactions();
+
+    res.then((txnList) {
+      setState(() {
+        _userTransactions = txnList;
+      });
+    });
+  }
+
+  void _showChartHandler(bool show) {
+    setState(() {
+      _showChart = show;
+    });
+  }
+
+  Future<void> _addNewTransaction(
+      String title, double amount, String category, DateTime chosenDate) async {
+    final newTxn = Transaction(
+      DateTime.now().millisecondsSinceEpoch.toString(),
+      title,
+      amount,
+      category,
+      chosenDate,
+    );
+    int res = await DatabaseHelper.instance.insert(newTxn);
+
+    if (res != 0) {
+      _updateUserTransactionsList();
+    }
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet<dynamic>(
+      isScrollControlled: true,
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext bc) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.80,
+          decoration: new BoxDecoration(
+            color: Colors.white,
+            borderRadius: new BorderRadius.only(
+              topLeft: const Radius.circular(25.0),
+              topRight: const Radius.circular(25.0),
+            ),
+          ),
+          child: NewTransactionForm(_addNewTransaction),
+        );
+      },
+    );
+  }
+
+  Future<void> _deleteTransaction(String id) async {
+    int parsedId =
+        int.tryParse(id) ?? 0; // Use 0 as default value if parsing fails
+    int res = await DatabaseHelper.instance.deleteTransactionById(parsedId);
+    if (res != 0) {
+      _updateUserTransactionsList();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final AppBar myAppBar = AppBar(
+      title: Text(
+        'Personal Expenses',
+        style: TextStyle(
+          fontFamily: "Quicksand",
+          fontWeight: FontWeight.w400,
+          fontSize: 20.0,
+        ),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context),
+          tooltip: "Add New Transaction",
+        ),
+      ],
+    );
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    final bool isLandscape =
+        mediaQueryData.orientation == Orientation.landscape;
+
+    final double availableHeight = mediaQueryData.size.height -
+        myAppBar.preferredSize.height -
+        mediaQueryData.padding.top -
+        mediaQueryData.padding.bottom;
+
+    final double availableWidth = mediaQueryData.size.width -
+        mediaQueryData.padding.left -
+        mediaQueryData.padding.right;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: myAppBar,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Show Chart",
+                    style: TextStyle(
+                      fontFamily: "Rubik",
+                      fontSize: 16.0,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                  Switch.adaptive(
+                    activeColor: Colors.amber[700],
+                    value: _showChart,
+                    onChanged: (value) => _showChartHandler(value),
+                  ),
+                ],
+              ),
+            if (isLandscape)
+              _showChart
+                  ? myChartContainer(
+                      height: availableHeight * 0.8,
+                      width: 0.6 * availableWidth)
+                  : myTransactionListContainer(
+                      height: availableHeight * 0.8,
+                      width: 0.6 * availableWidth),
+            if (!isLandscape)
+              myCircularChartContainer(
+                  height: availableHeight * 0.2, width: availableWidth),
+            //CategoryChart(_recentTransactions),
+            myChartContainer(
+                height: availableHeight * 0.3, width: availableWidth),
+            if (!isLandscape)
+              myTransactionListContainer(
+                  height: availableHeight * 0.7, width: availableWidth),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              child: Icon(Icons.add),
+              tooltip: "Add New Transaction",
+              onPressed: () => _startAddNewTransaction(context),
+            ),
+    );
+  }
+
+  Widget myCircularChartContainer(
+      {required double height, required double width}) {
+    return Container(
+      height: height,
+      width: width,
+      child: CategoryChart(_userTransactions),
+    );
+  }
+
+  Widget myChartContainer({required double height, required double width}) {
+    return Container(
+      height: height,
+      width: width,
+      child: Chart(_recentTransactions),
+    );
+  }
+
+  Widget myTransactionListContainer(
+      {required double height, required double width}) {
+    return Container(
+      height: height,
+      width: width,
+      child: TransactionList(_userTransactions, _deleteTransaction),
+    );
+  }
+}
